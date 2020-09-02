83ab1e7d4aeb753820087bfeec605ad4b7593a11
Sergio Lucena <sergio.lucena@raisin.com>
2020-09-02 21:43:35 +0200

    Feat(Button): Creates the component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Button } from "components/common";
 
 function App() {
   return (
-    <h1>App</h1>
+    <>
+      <h1>App</h1>
+      <Button>click me!</Button>
+    </>
   );
 }
 
