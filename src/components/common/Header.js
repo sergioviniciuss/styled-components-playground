95f90a7959b23773fbffad2b0a65bdacfa8ff408
Sergio Lucena <sergio.lucena@raisin.com>
2020-09-02 23:42:08 +0200

    Feat(Header): Creates the header with the links

---
+++ src/components/common/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const HeaderWrapper = styled.header`
+height: 60px;
+width: 100%;
+box-sizing: border-box;
+display: flex;
+padding: 0 16px;
+top: 0;
+background: #eee;
+
+`;
+
+const Header = () => {
+  return <HeaderWrapper>
+    <Link to='/'>Home</Link>
+    <Link to='/login'>Login</Link>
+  </HeaderWrapper>;
+};
+
+export {Header};
