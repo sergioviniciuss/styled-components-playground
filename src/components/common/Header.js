92069fe8ce44eb7897e8b806c79f66601efa5033
sergioviniciuss <sergioviniciuss@gmail.com>
2020-09-03 20:00:35 +0200

    Style(header): Formats code with prettier

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,21 +3,22 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const HeaderWrapper = styled.header`
-height: 60px;
-width: 100%;
-box-sizing: border-box;
-display: flex;
-padding: 0 16px;
-top: 0;
-background: #eee;
-
+  height: 60px;
+  width: 100%;
+  box-sizing: border-box;
+  display: flex;
+  padding: 0 16px;
+  top: 0;
+  background: #eee;
 `;
 
 const Header = () => {
-  return <HeaderWrapper>
-    <Link to='/'>Home</Link>
-    <Link to='/login'>Login</Link>
-  </HeaderWrapper>;
+  return (
+    <HeaderWrapper>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+    </HeaderWrapper>
+  );
 };
 
-export {Header};
+export { Header };
