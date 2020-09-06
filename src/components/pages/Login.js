07febe8f01ea002f42094880147fe56078ce93de
sergioviniciuss <sergioviniciuss@gmail.com>
2020-09-06 23:26:39 +0200

    Feat(Login): References Button/Input styled components within another styled component

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -16,6 +16,14 @@ const Form = styled.form`
     text-align: center;
     margin: 10px 0;
   }
+
+  >${Button}:first-of-type{
+    margin-top: 40px;
+  }
+
+  >${Input}{
+    margin-top: 20px;
+  }
 `;
 
 let timeout;
@@ -57,6 +65,7 @@ export default () => {
           <Spinner />
         ) : (
           <>
+          <span>Login if you have an account</span>
             <Input
               name="username"
               placeholder="Username"
