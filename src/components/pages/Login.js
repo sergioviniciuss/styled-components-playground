4779115fbeec2bd696358ba728f8452ff89d95c3
sergioviniciuss <sergioviniciuss@gmail.com>
2020-09-06 21:18:37 +0200

    Feat(Login): Includes Login/Register buttons and Spinner icon animation

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { PageLayout, Input, PasswordInput } from "components/common";
+import React, { useState, useEffect } from "react";
+import { PageLayout, Input, PasswordInput, Button, Spinner } from "components/common";
 import styled from "styled-components";
 
 const Form = styled.form`
@@ -11,11 +11,18 @@ const Form = styled.form`
   padding: 16px;
   color: black;
   border-radius: 4px;
+
+  .alt-text {
+    text-align: center;
+    margin: 10px 0;
+  }
 `;
 
+let timeout;
 
 export default () => {
   const [formFields, setFormFields] = useState({ username: "", password: "" });
+  const [loading, setLoading] = useState(false);
 
   const handleInputChange = (e) => {
     e.persist();
@@ -26,22 +33,55 @@ export default () => {
     }));
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    timeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    };
+  }, []);
+
   return (
     <PageLayout>
       <h1>Login</h1>
-      <Form>
-        <Input
-          name="username"
-          placeholder="Username"
-          type="text"
-          onChange={handleInputChange}
-          value={formFields.username}
-        />
-        <PasswordInput
-          name="password"
-          onChange={handleInputChange}
-          value={formFields.password}
-        />
+      <Form onSubmit={handleSubmit}>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <Input
+              name="username"
+              placeholder="Username"
+              type="text"
+              onChange={handleInputChange}
+              value={formFields.username}
+            />
+            <PasswordInput
+              name="password"
+              onChange={handleInputChange}
+              value={formFields.password}
+            />
+          </>
+        )}
+        <Button large type="submit" disabled={loading}>
+          {loading ? "Loading..." : "Login"}
+        </Button>
+        {!loading && (
+          <>
+            <div className="alt-text">or</div>
+            <Button secondary type="button">
+              Register
+            </Button>
+          </>
+        )}
       </Form>
     </PageLayout>
   );
