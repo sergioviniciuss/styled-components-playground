2ad9866d1d67810b6b009dcc23210e41d2c0bc92
sergioviniciuss <sergioviniciuss@gmail.com>
2020-09-06 19:15:36 +0200

    Feat(Login): Adds the login form using the input components

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import { PageLayout } from 'components/common';
+import React, { useState } from "react";
+import { PageLayout, Input } from "components/common";
+import styled from "styled-components";
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 400px;
+  background: white;
+  border: 1x solid #eee;
+  box-sizing: border-box;
+  padding: 16px;
+  color: black;
+  border-radius: 4px;
+`;
+
 
 export default () => {
+  const [formFields, setFormFields] = useState({ username: "", password: "" });
+
+  const handleInputChange = (e) => {
+    e.persist();
+    setFormFields((state) => ({
+      ...state,
+      [e.target.name]: e.target.value,
+      [e.target.password]: e.target.password,
+    }));
+  };
+
   return (
     <PageLayout>
-      Login
+      <h1>Login</h1>
+      <Form>
+        <Input
+          name="username"
+          placeholder="Username"
+          type="text"
+          onChange={handleInputChange}
+          value={formFields.username}
+        />
+        <Input
+          name="password"
+          placeholder="Password"
+          type="password"
+          onChange={handleInputChange}
+          value={formFields.password}
+        />
+      </Form>
     </PageLayout>
-  )
+  );
 };
